7cbe459e15284451582cf981d126af0b32515152
Kadir Çetin <kadir.cetin72@gmail.com>
2026-06-22 10:24:02 +0300

    ItemDao interface and ItemDatabase class are added

---
+++ app/src/main/java/com/kadircetin/smartshopping/roomdb/ItemDatabase.kt
@@ -0,0 +1,10 @@
+package com.kadircetin.smartshopping.roomdb
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.kadircetin.smartshopping.model.Item
+
+@Database(entities = [Item::class], version = 1)
+abstract class ItemDatabase : RoomDatabase() {
+    abstract fun itemDao(): ItemDao
+}
