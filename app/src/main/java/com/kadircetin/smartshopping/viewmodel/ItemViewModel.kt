080824696aaa4dda1ef02228375f493eed92fbf8
Kadir Çetin <kadir.cetin72@gmail.com>
2026-06-22 16:02:13 +0300

    ItemViewModel is added

---
+++ app/src/main/java/com/kadircetin/smartshopping/viewmodel/ItemViewModel.kt
@@ -0,0 +1,51 @@
+package com.kadircetin.smartshopping.viewmodel
+
+import android.app.Application
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.room.Room
+import com.kadircetin.smartshopping.model.Item
+import com.kadircetin.smartshopping.roomdb.ItemDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ItemViewModel(application: Application) : AndroidViewModel(application) {
+    private val db = Room.databaseBuilder(
+        getApplication(),
+        ItemDatabase::class.java,
+        "Items"
+    ).build()
+
+    private val itemDao = db.itemDao()
+
+    val itemList = mutableStateOf<List<Item>>(listOf())
+    val selectedItem = mutableStateOf<Item>(Item("", "", "", ByteArray(1)))
+
+    fun getItemList() {
+        viewModelScope.launch(Dispatchers.IO) {
+            itemList.value = itemDao.getItemWithNameAndId()
+        }
+    }
+
+    fun getItem(id: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val item = itemDao.getItemById(id)
+            item?.let {
+                selectedItem.value = it
+            }
+        }
+    }
+
+    fun insertItem(item: Item) {
+        viewModelScope.launch(Dispatchers.IO) {
+            itemDao.insert(item)
+        }
+    }
+
+    fun deleteItem(item: Item) {
+        viewModelScope.launch(Dispatchers.IO) {
+            itemDao.delete(item)
+        }
+    }
+}
